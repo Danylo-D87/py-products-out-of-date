f24fcdb065359a0cf41285b2f3d5a4646f6e619e
Danylo-D87 <quaestor88@gmail.com>
2025-05-01 15:08:03 +0300

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+import datetime
+import pytest
+from typing import List, Dict
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2025, 4, 30),
+                    "price": 600
+                },
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2025, 5, 5),
+                    "price": 120
+                },
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2025, 5, 1),
+                    "price": 160
+                }
+            ],
+            ["salmon"],
+        )
+    ]
+)
+def test_outdated_products(products: List[Dict[str, any]],
+                           expected: List[str]) -> None:
+    assert outdated_products(products) == expected
